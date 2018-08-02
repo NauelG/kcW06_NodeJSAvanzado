60a420689b4727b2c6f551ec4026a7bcf758a8ef
NauelG <g.nauel@gmail.com>
2018-08-02 13:00:05 +0200

    Creo método listar en el modelo

--- nodepop/models/Anuncio.js
+++ nodepop/models/Anuncio.js
@@ -2,6 +2,8 @@
 
 const mongoose = require('mongoose');
 
+// Modelo
+
 const anuncioSchema = mongoose.Schema({
     nombre: String,
     venta: Boolean,
@@ -9,6 +11,43 @@ const anuncioSchema = mongoose.Schema({
     foto: String,
     tags: [String]
 });
+
+// Métodos
+anuncioSchema.statics.listar = function(request) {
+    const name = request.query.name;
+    const tags = request.query.tags;
+    const type = request.query.type;
+    const range = request.query.range;
+    const limit = parseInt(request.query.limit);
+    const skip = parseInt(request.query.skip);
+    const sort = request.query.sort;
+
+
+    const filtro = {};
+    if (name) {
+        filtro.nombre = { $regex: `.*${name}`, $options: "i" }
+    };
+    if (tags) {
+        filtro.tags = tags
+    };
+    if (range) {
+        const rangeArray = range.split('-');
+        filtro.precio = { $gte: parseInt(rangeArray[0]), $lte: parseInt(rangeArray[1]) }
+    };
+    if (type === 'venta') {
+        filtro.venta = true
+    };
+    if (type === 'compra') {
+        filtro.venta = false
+    }
+    console.log(filtro)
+
+    const query = Anuncio.find(filtro);
+    query.limit(limit);
+    query.skip(skip);
+    query.sort(sort);
+    return query.exec();
+};
 
 const Anuncio = mongoose.model('Anuncio', anuncioSchema);
 
