ef81459a35de918da06f483ee6e9bb9e04ae9d89
NauelG <g.nauel@gmail.com>
2018-08-02 14:13:09 +0200

    Implemento primera versión del post y envuelvo los await en try-catch para capturar errores

--- nodepop/routes/api/anuncios.js
+++ nodepop/routes/api/anuncios.js
@@ -7,28 +7,47 @@ const Anuncio = require('../../models/Anuncio');
 
 // GET - Devuelve una lista con todos los anuncios
 router.get('/', async(req, res, next) => {
-
-    const anunciosResult = await Anuncio.listar(req);
-    console.log(anunciosResult);
-    res.json({ success: true, result: anunciosResult });
+    try {
+        const anunciosResult = await Anuncio.listar(req);
+        console.log(anunciosResult);
+        res.json({ success: true, result: anunciosResult });
+    } catch (err) {
+        next(err);
+    }
 });
 
 // GET Devuelve una lista de tags
 router.get('/tags', async(req, res, next) => {
-
-    const tagsResults = await Anuncio.getTags();
-    const tagsReturn = [];
-    tagsResults.forEach(result => {
-        result.tags.forEach(tag => {
-            if (tagsReturn.indexOf(tag) === -1) {
-                tagsReturn.push(tag);
-            };
+    try {
+        const tagsResults = await Anuncio.getTags();
+        const tagsReturn = [];
+        tagsResults.forEach(result => {
+            result.tags.forEach(tag => {
+                if (tagsReturn.indexOf(tag) === -1) {
+                    tagsReturn.push(tag);
+                };
+            });
         });
-    });
-    res.json({ success: true, result: tagsReturn });
+        res.json({ success: true, result: tagsReturn });
+    } catch (err) {
+        next(err);
+    }
 });
 
 // POST Crea un nuevo anuncio
+router.post('/', async(req, res, next) => {
+    try {
+        const datosAnuncio = req.body;
+
+        const anuncio = new Anuncio(datosAnuncio);
+
+        anuncioCreado = await anuncio.save();
+
+        res.json({ success: true, result: anuncioCreado });
+    } catch (err) {
+        next(err);
+    }
+});
 
 
 
